7d43fe41cef5de9f56b28b757f75037b9086098d
35mohitgupta] <35mohitgupta@gmail.com>
2020-09-06 11:04:42 +0530

    added docker-compose for testing and production dockerfile

--- my-docker-app/src/App.js
+++ my-docker-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to react-app developed by mohitfor docker demo
+          Welcome to react-app developed by mohit
         </p>
         <a
           className="App-link"
